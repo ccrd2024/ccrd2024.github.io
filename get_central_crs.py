dfb63b3c094bccac2d7e4c4abbe78479942df909
a <waosp2014@proton.me>
2024-03-07 22:14:36 +0800

    upload.

---
+++ get_central_crs.py
@@ -0,0 +1,64 @@
+import os,re,json
+def clean(a):
+    return a if len(a)<61 else '%s...'%a[:61]
+def clean2(a):
+    return a if len(a)<13 else '%s...'%a[:13]
+ks=['江青','云鹤','文元','洪文','春桥']
+if not os.path.exists(p1:='中央文革CCRD文库全集（标题提及）'):
+    os.mkdir(p1)
+if not os.path.exists(p2:='中央文革CCRD文库全集（内容提及）'):
+    os.mkdir(p2)
+for a in os.walk('HTML'):
+    a[2].sort()
+    lf=[b for b in a[2]if re.search(r'\d{6}',b)]
+    for b in lf:
+        f=open('%s/%s'%(a[0],b),'r');t=f.read();f.close()
+        f=open('%s/%s.json'%(a[0].replace('HTML','JSON'),'.'.join(b.split('.')[:-1])),'r');js=json.loads(f.read());f.close()
+        k=0
+        for c in ks:
+            if js['title'].find(c)!=-1:
+                k=1
+        for c in ks:
+            if k==0 and t.find(c)!=-1:
+                k=2
+        ti='%s-%s%s-%s-%s.htm'%(js['date'].replace('/','-'),
+                             clean(js['title']).replace('/','-'),
+                             clean2('-%s'%','.join(js['authors'])if js['authors']else'').replace('/','-'),
+                             '-'.join(a[0].split('/')[1:]),
+                             '.'.join(b.split('.')[:-1]))
+        if k==1:
+            f=open(dst:='%s/%s'%(p1,ti),'w+');f.write(t);f.close()
+            print(dst)
+        elif k==2:
+            f=open(dst:='%s/%s'%(p2,ti),'w+');f.write(t);f.close()
+            print(dst)
+
+ks=['远新']
+if not os.path.exists(p1:='毛远新CCRD文库全集（标题提及）'):
+    os.mkdir(p1)
+if not os.path.exists(p2:='毛远新CCRD文库全集（内容提及）'):
+    os.mkdir(p2)
+for a in os.walk('HTML'):
+    a[2].sort()
+    lf=[b for b in a[2]if re.search(r'\d{6}',b)]
+    for b in lf:
+        f=open('%s/%s'%(a[0],b),'r');t=f.read();f.close()
+        f=open('%s/%s.json'%(a[0].replace('HTML','JSON'),'.'.join(b.split('.')[:-1])),'r');js=json.loads(f.read());f.close()
+        k=0
+        for c in ks:
+            if js['title'].find(c)!=-1:
+                k=1
+        for c in ks:
+            if k==0 and t.find(c)!=-1:
+                k=2
+        ti='%s-%s%s-%s-%s.htm'%(js['date'].replace('/','-'),
+                             clean(js['title']).replace('/','-'),
+                             clean2('-%s'%','.join(js['authors'])if js['authors']else'').replace('/','-'),
+                             '-'.join(a[0].split('/')[1:]),
+                             '.'.join(b.split('.')[:-1]))
+        if k==1:
+            f=open(dst:='%s/%s'%(p1,ti),'w+');f.write(t);f.close()
+            print(dst)
+        elif k==2:
+            f=open(dst:='%s/%s'%(p2,ti),'w+');f.write(t);f.close()
+            print(dst)
